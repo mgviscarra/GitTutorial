1604975779f3337982813744eddd4aee84cf3de1
Mauricio Viscarra <mgviscarra@gmail.com>
2026-06-15 18:51:12 -0400

    Adding first files

---
+++ file.ts
@@ -0,0 +1,16 @@
+//Some code here
+export class Path {
+    private path: string;
+
+    constructor(path: string) {
+        this.path = path;
+    }
+
+    public getPath(): string {
+        return this.path;
+    }
+
+    public setPath(path: string): void {
+        this.path = path;
+    }
+}
